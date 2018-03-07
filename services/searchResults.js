c5a3aeb42988e55e8ddfd0fd214879352aa34127
DylanCommean01 <DylanCommean01@yahoo.com>
2018-03-07 09:24:17 -0600

    another console.log cleanup.

--- services/searchResults.js
+++ services/searchResults.js
@@ -2,18 +2,14 @@ import * as baseService from './base';
 
 
 async function getArtistLocation(location) {
-    console.log(location);
     return baseService.get(`/api/artist/` + location);
 }
 
 async function getArtistInstrument(instrument) {
-    console.log(instrument);
     return baseService.get(`/api/artist/instrument/` + instrument);
 }
 
 async function getArtist(location, instrument) {
-    console.log(location);
-    console.log(instrument);
     return
     baseService.getMore(`/api/artist/both/`, { location: location, instrument: instrument });
     // baseService.get(`/api/artist/instrument/` + instrument);
